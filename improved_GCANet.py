8937e45d2707b1441b292748f6310a275e9528c2
17301076phc <17301076@bjtu.edu.cn>
2021-04-26 20:41:00 +0800

    committ

--- improved_GCANet.py
+++ improved_GCANet.py
@@ -39,69 +39,6 @@ class SmoothDilatedResidualBlock(nn.Module):
         return F.relu(x + y)
 
 
-# class SELayer(nn.Module):
-#     def __init__(self, channel, reduction=16):
-#         super(SELayer, self).__init__()
-#         self.avg_pool = nn.AdaptiveAvgPool2d(1)
-#         self.fc = nn.Sequential(
-#             nn.Linear(channel, channel // reduction, bias=False),
-#             nn.ReLU(inplace=True),
-#             nn.Linear(channel // reduction, channel, bias=False),
-#             nn.Sigmoid()
-#         )
-#
-#     def forward(self, x):
-#         b, c, _, _ = x.size()
-#         y = self.avg_pool(x).view(b, c)
-#         y = self.fc(y).view(b, c, 1, 1)
-#         return x * y.expand_as(x)
-#
-# class SpatialAttention(nn.Module):
-#     def __init__(self, kernel_size=7):
-#         super(SpatialAttention, self).__init__()
-#
-#         assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
-#         padding = 3 if kernel_size == 7 else 1
-#
-#         self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
-#         self.sigmoid = nn.Sigmoid()
-#
-#     def forward(self, x):
-#         avg_out = torch.mean(x, dim=1, keepdim=True)
-#         max_out, _ = torch.max(x, dim=1, keepdim=True)
-#         x = torch.cat([avg_out, max_out], dim=1)
-#         x = self.conv1(x)
-#         return self.sigmoid(x)
-#
-# class ChannelAttention(nn.Module):
-#     def __init__(self, in_planes, ratio=16):
-#         super(ChannelAttention, self).__init__()
-#         self.avg_pool = nn.AdaptiveAvgPool2d(1)
-#         self.max_pool = nn.AdaptiveMaxPool2d(1)
-#
-#         self.fc1   = nn.Conv2d(in_planes, in_planes // 16, 1, bias=False)
-#         self.relu1 = nn.ReLU()
-#         self.fc2   = nn.Conv2d(in_planes // 16, in_planes, 1, bias=False)
-#
-#         self.sigmoid = nn.Sigmoid()
-#
-#     def forward(self, x):
-#         avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
-#         max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
-#         out = avg_out + max_out
-#         return self.sigmoid(out)
-#
-# class CBAM(nn.Module):
-#     def __init__(self, channel):
-#         super(CBAM, self).__init__()
-#         self.channel_attention = ChannelAttention(channel)
-#         self.spatial_attention = SpatialAttention()
-#
-#     def forward(self, x):
-#         out = self.channel_attention(x) * x
-#         out = self.spatial_attention(out) * out
-#         return out
-
 class PALayer(nn.Module):
     def __init__(self, channel):
         super(PALayer, self).__init__()
@@ -161,15 +98,12 @@ class ResidualBlock(nn.Module):
 class GCANet(nn.Module):
     def __init__(self, in_c=4, out_c=3, only_residual=True):
         super(GCANet, self).__init__()
-        self.conv1 = nn.Conv2d(in_c, 64, 3, 1, 1, bias=False)  # 卷积 (通道数，输出通道数，卷积核尺寸，步长，填充，扩张，卷积核个数)
-        self.norm1 = nn.InstanceNorm2d(64, affine=True)  # 归一化
+        self.conv1 = nn.Conv2d(in_c, 64, 3, 1, 1, bias=False)  
+        self.norm1 = nn.InstanceNorm2d(64, affine=True)  
         self.conv2 = nn.Conv2d(64, 64, 3, 1, 1, bias=False)
         self.norm2 = nn.InstanceNorm2d(64, affine=True)
         self.conv3 = nn.Conv2d(64, 64, 3, 2, 1, bias=False)
         self.norm3 = nn.InstanceNorm2d(64, affine=True)
-
-        # self.cbam = CBAM(64)
-        # self.se = SELayer(64)
 
         self.res1 = SmoothDilatedResidualBlock(64, dilation=2)
         self.res2 = SmoothDilatedResidualBlock(64, dilation=2)
@@ -181,10 +115,9 @@ class GCANet(nn.Module):
 
         self.gate = nn.Conv2d(64 * 3, 3, 3, 1, 1, bias=True)
 
-        self.deconv3 = nn.ConvTranspose2d(64, 64, 4, 2, 1)  # 反卷积
+        self.deconv3 = nn.ConvTranspose2d(64, 64, 4, 2, 1) 
         self.norm4 = nn.InstanceNorm2d(64, affine=True)
 
-        # -------sos----------
         self.dense_1 = nn.Sequential(
                     ResidualBlock(64),
                     ResidualBlock(64),
@@ -196,7 +129,6 @@ class GCANet(nn.Module):
                     ResidualBlock(64),
                     ResidualBlock(64)
                 )
-        # ---------sos-------------
 
         self.fa = FA(64)
         self.pa = PALayer(64)
@@ -210,7 +142,7 @@ class GCANet(nn.Module):
 
     def forward(self, x):
         y = F.relu(self.norm1(self.conv1(x)))
-        z1 = y  # sos
+        z1 = y  
         y = F.relu(self.norm2(self.conv2(y)))
         z2 = y
         y1 = F.relu(self.norm3(self.conv3(y)))
@@ -229,26 +161,21 @@ class GCANet(nn.Module):
         y = self.fa(y)
         y3 = self.res7(y)
 
-        gates = self.gate(torch.cat((y1, y2, y3), dim=1))  # cat是将两个向量拼接在一起
+        gates = self.gate(torch.cat((y1, y2, y3), dim=1)) 
         gated_y = y1 * gates[:, [0], :, :] + y2 * gates[:, [1], :, :] + y3 * gates[:, [2], :, :]
         gated_y = self.pa(gated_y)
-        y = F.relu(self.norm4(self.deconv3(gated_y)))  # 反卷
+        y = F.relu(self.norm4(self.deconv3(gated_y))) 
 
-        # y4 = F.interpolate(y, y1.size()[2:], mode='bilinear', align_corners=True)  # sos
-        y4 = torch.add(y, z2)  # sos
-        y = self.dense_1(y4) - y  # sos
+        y4 = torch.add(y, z2) 
+        y = self.dense_1(y4) - y  
 
-        y = F.relu(self.norm5(self.deconv2(y)))  # 卷积
+        y = F.relu(self.norm5(self.deconv2(y)))  
+        y5 = torch.add(y, z1) 
+        y = self.dense_2(y5) - y  
 
-        # y5 = F.interpolate(y, z.size()[2:], mode='bilinear', align_corners=True)  # sos
-        y5 = torch.add(y, z1)  # sos
-        y = self.dense_2(y5) - y  # sos
-
-        # y = self.ca(y)
-        # y = self.pa(y)
 
         if self.only_residual:
-            y = self.deconv1(y)  # 卷积
+            y = self.deconv1(y) 
         else:
             y = F.relu(self.deconv1(y))
 
